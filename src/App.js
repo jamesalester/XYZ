b689869447bac7117688eea1ea903918ad3e6986
jamesalester <james@jameslester.xyz>
2020-06-22 16:59:11 +0100

    Removed old files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Home from "./containers/Home/Home";
-import Project from "./containers/Project/Project";
-import ScrollToTop from "./hoc/ScrollToTop";
-
-class App extends Component {
-  render() {
-    return (
-      <ScrollToTop>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/portfolio/:project" exact component={Project} />
-        </Switch>
-      </ScrollToTop>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
